1bc55491068ef74f0483b7024b44da4ad02636f8
Charlie Cook <turingbombe@gmail.com>
2016-07-28 13:20:24 -0400

    Self test complete Also trying out this larger description

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_sentence = self.split(/[?|!|.]/)
+    #binding.pry
+    split_sentence.delete("")
+    split_sentence.count
   end
 end
